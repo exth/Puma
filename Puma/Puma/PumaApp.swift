38dd24867adde32c992cd14beff09c5b3f6d04f6
Вадим <exth331@gmail.com>
2026-07-09 13:59:12 +0300

    Added Xcode project

---
+++ Puma/Puma/PumaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PumaApp.swift
+//  Puma
+//
+//  Created by Вадим on 09.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct PumaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
